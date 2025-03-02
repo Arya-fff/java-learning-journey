b541454ec55ad8d8b7d3d51e3450be384cd89e62
Arya-fff <aryazare2005@icloud.com>
2025-03-02 18:10:58 +0000

    substrings

---
+++ 01-basics/substring/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your email address: ");
+        String email = sc.nextLine();
+
+        if (email.contains("@")) {
+            String username = email.substring(0, email.indexOf("@"));
+            String domain = email.substring(email.indexOf("@") + 1);
+        } else {
+            System.out.println("Invalid email address");
+        }
+
+    }
+}
