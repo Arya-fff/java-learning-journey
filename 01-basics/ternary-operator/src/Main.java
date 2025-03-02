50b6c6acfc63a1bb4de9f3c5d6fd86a5bcc26eeb
Arya-fff <aryazare2005@icloud.com>
2025-03-02 21:00:56 +0000

    Ternary operator

---
+++ 01-basics/ternary-operator/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int time = 14;
+        String amOrpm = (time > 12) ? "AM" : "PM";
+        System.out.println(amOrpm);
+    }
+}
